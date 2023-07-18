54c65633c58c37a90c17ebd437cd1aeeba848161
dr-michael-haley <57752827+dr-michael-haley@users.noreply.github.com>
2023-07-18 15:08:09 +0100

    Update

--- Mike_IMC_Pipeline/utils.py
+++ Mike_IMC_Pipeline/utils.py
@@ -1,7 +1,13 @@
+import pandas as pd
+import anndata as ad
+
 import datetime 
 from types import ModuleType
-import pandas as pd
+
+import os
 from os import getlogin
+
+from pathlib import Path
 
 
 def compare_lists(L1, L2, L1_name, L2_name, return_error=True):
@@ -94,4 +100,92 @@ def adlog(adata,
         adata.write(temp_file)
         log.loc[len(log.index)] = [str(x) for x in [date_now, time_now, f'Saved AnnData backup: {temp_file}', None, None, login_user]]
 
+
+def subset(adata,
+         obs_column,
+         obs_values=[],
+         include_list=True):
+    
+    '''
+    Returns a filtered AnnData object
+    
+    obs_column - Column in .obs to filter on
+    obs_values - Values you want to include if include_list=True, or exlude if include_list=False
+    '''
+    
+    if include_list==True:
+        return adata[adata.obs[obs_column].isin(obs_values)]
+    else:
+        return adata[~adata.obs[obs_column].isin(obs_values)]
+        
+        
+def _cleanstring(data):
+    '''
+    Helper function that returns a clean string
+    '''
+    
+    import re
+    data = str(data)
+    
+    data = re.sub(r'\W+', '', data)
+    
+    return data
+
+def _save(path, filename):
+    '''
+    Helper function that returns a save path, and makes sure all the directories are in place
+    '''
+    
+    from pathlib import Path
+    
+    path_dir=Path(*path)
+    os.makedirs(path_dir, exist_ok=True)
+    
+    return Path(path_dir, filename)
+
+
+def _check_input_type(data):
+    '''
+    Helper function that will, depending on the datatype, either access the the adata.obs from an AnnData, take a dataframe directly, or load a .csv if given a string
+    '''    
+    
+    input_type = type(data)
+    
+    if input_type==ad._core.anndata.AnnData:
+        data = data.obs.copy()
+    
+    elif input_type==pd.core.frame.DataFrame:
+        data = data.copy()
+        
+    elif input_type==str:
+        data = pd.read_csv(data, low_memory=False)
+
+    else:
+        raise Exception(f'Data type {str(input_type)} not recognised')
+        
+    return data
+
+
+def _to_list(data):
+    '''
+    Helper function that sanitises an input so that its in a list format, even if just a list of one
+    '''
+    
+    import pandas as pd
+    
+    dtype = type(data)
+    
+    if dtype==pd.core.indexes.base.Index:
+        data=data.tolist()
+    
+    elif dtype==list:
+        pass
+    
+    elif dtype==type(None):
+        return []
+    
+    else:
+        data=[data]
+        
+    return data
         
