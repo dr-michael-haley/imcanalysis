969cd2c1dcff3c3cac6c9164ee10e4724df406a4
Michael Haley <michael.haley@manchester.ac.uk>
2026-01-21 12:32:02 +0000

    Update job_env.sh

--- SLURM_scripts/job_env.sh
+++ SLURM_scripts/job_env.sh
@@ -1,10 +1,6 @@
 # Environment used by IMC analysis jobs
 
-# This was causing a crash
-# module load gcc/13.3.0
 module purge
-
 export MPLBACKEND="Agg"
 export QT_QPA_PLATFORM="offscreen"
 unset DISPLAY
-export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:${LD_LIBRARY_PATH:-}"
