00f59805d9678cce55b88a6776dcc0ed073672ae
Michael Haley <57752827+dr-michael-haley@users.noreply.github.com>
2026-04-07 21:22:45 +0100

    Update job_batch_integration.sh

--- SLURM_scripts/job_batch_integration.sh
+++ SLURM_scripts/job_batch_integration.sh
@@ -19,7 +19,7 @@ source "$HOME/imcanalysis/SLURM_scripts/job_env.sh"
 
 echo "Batch integration job is using $SLURM_GPUS GPU(s) with ID(s) $CUDA_VISIBLE_DEVICES and $SLURM_NTASKS CPU core(s)"
 
-conda activate "${IMC_ENV_SEGMENTATION:-imc_segmentation}"
+conda activate "${IMC_ENV_CELLCHARTER:-imc_cellcharter}"
 # Fix ctypes error
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:${LD_LIBRARY_PATH:-}"
 
